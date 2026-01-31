c945ad42eca3d9384667314ea5f56434b80f94cc
CodieCCU <113255707+CodieCCU@users.noreply.github.com>
2026-01-31 14:15:09 -0800

    Create cmd/server/main.go with API handlers and WebSocket support

---
+++ cmd/server/main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"net/http"
+	"github.com/gorilla/websocket"
+)
+
+var upgrader = websocket.Upgrader{
+	CheckOrigin: func(r *http.Request) bool {
+		return true
+	},
+}
+
+func main() {
+	http.HandleFunc("/ws", handleWebSocket)
+	http.ListenAndServe(":8080", nil)
+}
+
+func handleWebSocket(w http.ResponseWriter, r *http.Request) {
+	conn, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		http.Error(w, "Could not upgrade connection", http.StatusBadRequest)
+		return
+	}
+	defer conn.Close()
+
+	// Handle WebSocket communication
+	for {
+		messageType, msg, err := conn.ReadMessage()
+		if err != nil {
+			break
+		}
+		// Handle incoming message
+		if err := conn.WriteMessage(messageType, msg); err != nil {
+			break
+		}
+	}
+}
